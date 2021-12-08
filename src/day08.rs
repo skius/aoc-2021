5b9c5a9dd6b7f064bd519d2d66566d554abf6705
Niels Saurer <me@nielssaurer.com>
2021-12-08 16:34:19 +0100

    day08 bitvectors

--- src/day08.rs
+++ src/day08.rs
@@ -1,5 +1,6 @@
 use std::collections::{HashMap, HashSet};
 use std::io::{BufRead, BufReader, Read};
+use std::ops::Not;
 
 pub fn part1(input: &mut dyn Read) -> String {
     let mut buf = BufReader::new(input);
@@ -28,22 +29,40 @@ pub fn part1(input: &mut dyn Read) -> String {
     total.to_string()
 }
 
+fn char_to_onehot(c: char) -> u8 {
+    1 << (c as u8 - 'a' as u8)
+}
+
+fn onehot_to_char(onehot: u8) -> char {
+    ((onehot as f64).log2() as u8 + ('a' as u8)) as char
+}
+
+fn set_diff(a: u8, b: u8) -> u8 {
+    a & !b
+}
+
+fn set_union(a: u8, b: u8) -> u8 {
+    a | b
+}
+
+
+
 pub fn part2(input: &mut dyn Read) -> String {
     let mut buf = BufReader::new(input);
     let mut lines = buf.lines();
     let mut total = 0;
 
-    let mut numbers_to_original_wires = Vec::<HashSet<char>>::new();
-    numbers_to_original_wires.insert(0, "abcefg".chars().collect());
-    numbers_to_original_wires.insert(1, "cf".chars().collect());
-    numbers_to_original_wires.insert(2, "acdeg".chars().collect());
-    numbers_to_original_wires.insert(3, "acdfg".chars().collect());
-    numbers_to_original_wires.insert(4, "bdcf".chars().collect());
-    numbers_to_original_wires.insert(5, "abdfg".chars().collect());
-    numbers_to_original_wires.insert(6, "abdefg".chars().collect());
-    numbers_to_original_wires.insert(7, "acf".chars().collect());
-    numbers_to_original_wires.insert(8, "abcdefg".chars().collect());
-    numbers_to_original_wires.insert(9, "abcdfg".chars().collect());
+    let mut numbers_to_original_wires = [0; 10];
+    numbers_to_original_wires[0] = "abcefg".chars().map(char_to_onehot).fold(0, |acc, curr| acc | curr);
+    numbers_to_original_wires[1] = "cf".chars().map(char_to_onehot).fold(0, |acc, curr| acc | curr);
+    numbers_to_original_wires[2] = "acdeg".chars().map(char_to_onehot).fold(0, |acc, curr| acc | curr);
+    numbers_to_original_wires[3] = "acdfg".chars().map(char_to_onehot).fold(0, |acc, curr| acc | curr);
+    numbers_to_original_wires[4] = "bdcf".chars().map(char_to_onehot).fold(0, |acc, curr| acc | curr);
+    numbers_to_original_wires[5] = "abdfg".chars().map(char_to_onehot).fold(0, |acc, curr| acc | curr);
+    numbers_to_original_wires[6] = "abdefg".chars().map(char_to_onehot).fold(0, |acc, curr| acc | curr);
+    numbers_to_original_wires[7] = "acf".chars().map(char_to_onehot).fold(0, |acc, curr| acc | curr);
+    numbers_to_original_wires[8] = "abcdefg".chars().map(char_to_onehot).fold(0, |acc, curr| acc | curr);
+    numbers_to_original_wires[9] = "abcdfg".chars().map(char_to_onehot).fold(0, |acc, curr| acc | curr);
 
     for line in lines {
         let line = line.unwrap();
@@ -54,7 +73,7 @@ pub fn part2(input: &mut dyn Read) -> String {
         let input_displays = input_part.trim().split(" ").collect::<Vec<_>>();
         let output_displays = output_part.trim().split(" ").collect::<Vec<_>>();
 
-        println!("\n______________\n{}", line);
+        // println!("\n______________\n{}", line);
 
         // for display in &output_displays {
         //     let n = display.len();
@@ -66,7 +85,7 @@ pub fn part2(input: &mut dyn Read) -> String {
         // }
 
         // which wires are in which number
-        let mut wires_in_number = [0; 10].map(|_| HashSet::<char>::new());
+        let mut wires_in_number = [0; 10];//.map(|_| HashSet::<char>::new());
         // for each number, which current wire maps to which original wire
         let mut curr_to_original = HashMap::<char, char>::new();
 
@@ -74,34 +93,34 @@ pub fn part2(input: &mut dyn Read) -> String {
             let n = display.len();
             // don't need to loop over all chars, can just set equal to .collect
             for c in display.chars() {
+                let c = char_to_onehot(c);
                 match n {
                     2 | 3 | 4 | 7 => {
-                        let num = HashMap::from([
-                            (2, 1),
-                            (3, 7),
-                            (4, 4),
-                            (7, 8),
-                        ])[&n];
-                        wires_in_number[num].insert(c);
+                        let num = match n {
+                            2 => 1,
+                            3 => 7,
+                            4 => 4,
+                            7 => 8,
+                            _ => unreachable!(),
+                        };
+                        *wires_in_number.get_mut(num).unwrap() |= c;
                         // orders[num].insert(c, c);
                     }
                     _ => {}
                 }
             }
         }
-        println!("Hashsets: {:?}", wires_in_number);
-
-        assert!(wires_in_number[1].len() != 0);
-        assert!(wires_in_number[7].len() != 0);
 
         // the wire a must be 7's wires minus 1's wires
-        let a = wires_in_number[7].difference(&wires_in_number[1]).copied().collect::<HashSet<_>>();
-        let curr_a = *a.iter().next().unwrap();
-        curr_to_original.insert(curr_a, 'a');
+        let a = set_diff(wires_in_number[7], wires_in_number[1]);
+        // let curr_a = *a.iter().next().unwrap();
+        curr_to_original.insert(onehot_to_char(a), 'a');
 
-        let c_and_f = wires_in_number[7].difference(&a).copied().collect::<HashSet<_>>();
-        let b_and_d = wires_in_number[4].difference(&c_and_f).copied().collect::<HashSet<_>>();
-        let e_and_g = wires_in_number[8].difference(&b_and_d.union(&a.union(&c_and_f).copied().collect()).copied().collect()).copied().collect::<HashSet<_>>();
+        let c_and_f = set_diff(wires_in_number[7], a);
+        let b_and_d = set_diff(wires_in_number[4], c_and_f);
+        let e_and_g = set_diff(wires_in_number[8], set_union(b_and_d, set_union(a, c_and_f)));
+        // let b_and_d = wires_in_number[4].difference(&c_and_f).copied().collect::<HashSet<_>>();
+        // let e_and_g = wires_in_number[8].difference(&b_and_d.union(&a.union(&c_and_f).copied().collect()).copied().collect()).copied().collect::<HashSet<_>>();
 
 
         // now a run to treat 0,6,9:
@@ -111,10 +130,10 @@ pub fn part2(input: &mut dyn Read) -> String {
                 continue;
             }
 
-            let wires = display.chars().collect::<HashSet<_>>();
+            let wires = display.chars().map(char_to_onehot).fold(0, |acc, onehot| acc | onehot);
             // if it's just missing e, then it's 9
-            let diff_e_g = wires.difference(&e_and_g).collect::<HashSet<_>>();
-            let diff_len = diff_e_g.len();
+            let diff_e_g = set_diff(wires,e_and_g);
+            let diff_len = diff_e_g.count_ones();
             if diff_len == 5 {
                 // only 9 will still have 5 left over
                 wires_in_number[9] = wires;
@@ -122,8 +141,8 @@ pub fn part2(input: &mut dyn Read) -> String {
             }
 
             // if it's just missing c, then it's 9
-            let diff_c_f = wires.difference(&c_and_f).collect::<HashSet<_>>();
-            let diff_len = diff_c_f.len();
+            let diff_c_f = set_diff(wires, c_and_f);
+            let diff_len = diff_c_f.count_ones();
             if diff_len == 5 {
                 // only 6 will still have 5 left over
                 wires_in_number[6] = wires;
@@ -131,8 +150,8 @@ pub fn part2(input: &mut dyn Read) -> String {
             }
 
             // if it's just missing d, then it's 9
-            let diff_b_d = wires.difference(&b_and_d).collect::<HashSet<_>>();
-            let diff_len = diff_b_d.len();
+            let diff_b_d = set_diff(wires, b_and_d);
+            let diff_len = diff_b_d.count_ones();
             if diff_len == 5 {
                 // only 0 will still have 5 left over
                 wires_in_number[0] = wires;
@@ -141,26 +160,26 @@ pub fn part2(input: &mut dyn Read) -> String {
         }
 
         // we have more mappings now
-        let d = b_and_d.difference(&wires_in_number[0]).copied().collect::<HashSet<_>>();
-        curr_to_original.insert(*d.iter().next().unwrap(), 'd');
-        let b = b_and_d.difference(&d).copied().collect::<HashSet<_>>();
-        curr_to_original.insert(*b.iter().next().unwrap(), 'b');
+        let d = set_diff(b_and_d, wires_in_number[0]);
+        curr_to_original.insert(onehot_to_char(d), 'd');
+        let b = set_diff(b_and_d, d);
+        curr_to_original.insert(onehot_to_char(b), 'b');
 
-        let e = e_and_g.difference(&wires_in_number[9]).copied().collect::<HashSet<_>>();
-        curr_to_original.insert(*e.iter().next().unwrap(), 'e');
-        let g = e_and_g.difference(&e).copied().collect::<HashSet<_>>();
-        curr_to_original.insert(*g.iter().next().unwrap(), 'g');
+        let e = set_diff(e_and_g, wires_in_number[9]);
+        curr_to_original.insert(onehot_to_char(e), 'e');
+        let g = set_diff(e_and_g, e);
+        curr_to_original.insert(onehot_to_char(g), 'g');
 
-        let c = c_and_f.difference(&wires_in_number[6]).copied().collect::<HashSet<_>>();
-        curr_to_original.insert(*c.iter().next().unwrap(), 'c');
-        let f = c_and_f.difference(&c).copied().collect::<HashSet<_>>();
-        curr_to_original.insert(*f.iter().next().unwrap(), 'f');
+        let c = set_diff(c_and_f, wires_in_number[6]);
+        curr_to_original.insert(onehot_to_char(c), 'c');
+        let f = set_diff(c_and_f, c);
+        curr_to_original.insert(onehot_to_char(f), 'f');
 
         // now we have all the mappings
         let mut curr_total = 0;
         for display in &output_displays {
-            let wires = display.chars().map(|c| curr_to_original[&c]).collect::<HashSet<_>>();
-            println!("output_display curr wires: {}, supposed original: {}", display, wires.iter().collect::<String>());
+            let wires = display.chars().map(|c| curr_to_original[&c]).map(char_to_onehot).fold(0, |acc, onehot| acc | onehot);
+            // println!("output_display curr wires: {}, supposed original: {}", display, wires.iter().collect::<String>());
 
             let (idx, _) = numbers_to_original_wires.iter().enumerate().find(|(idx, original_wires)| &wires == *original_wires).unwrap();
             curr_total *= 10;
