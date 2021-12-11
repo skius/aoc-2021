d31b728e81660622b511d7595eff75a17134c2c9
Niels Saurer <me@nielssaurer.com>
2021-12-11 18:07:58 +0100

    day11

--- src/day10.rs
+++ src/day10.rs
@@ -1,9 +1,133 @@
-use std::io::Read;
+use std::io::{BufRead, BufReader, Read};
 
-pub fn part1(_input: &mut dyn Read) -> String {
-    todo!()
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+enum Kind {
+    Square, // []
+    Round, // ()
+    Curly, // {}
+    Angle, // <>
 }
 
-pub fn part2(_input: &mut dyn Read) -> String {
-    todo!()
+impl Kind {
+    fn syntax_score(&self) -> usize {
+        match self {
+            Kind::Square => 57,
+            Kind::Round => 3,
+            Kind::Curly => 1197,
+            Kind::Angle => 25137,
+        }
+    }
+
+    fn auto_score(&self) -> usize {
+        match self {
+            Kind::Square => 2,
+            Kind::Round => 1,
+            Kind::Curly => 3,
+            Kind::Angle => 4,
+        }
+    }
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+enum Direction {
+    Open(Kind),
+    Closed(Kind),
+}
+
+fn get_inputs(input: &mut dyn Read) -> Vec<Vec<Direction>> {
+    let mut buf = BufReader::new(input);
+    let mut lines = buf.lines();
+    lines.map(|l| {
+        let l = l.unwrap();
+        l.chars().map(|c| {
+            match c {
+                '<' => Direction::Open(Kind::Angle),
+                '>' => Direction::Closed(Kind::Angle),
+                '(' => Direction::Open(Kind::Round),
+                ')' => Direction::Closed(Kind::Round),
+                '{' => Direction::Open(Kind::Curly),
+                '}' => Direction::Closed(Kind::Curly),
+                '[' => Direction::Open(Kind::Square),
+                ']' => Direction::Closed(Kind::Square),
+                _ => panic!("Unknown character: {}", c),
+            }
+        }).collect()
+    }).collect()
+}
+
+pub fn part1(input: &mut dyn Read) -> String {
+    let stacks = get_inputs(input);
+
+    let mut total = 0;
+    'outer: for line in stacks {
+        let mut stack = Vec::new();
+        for dir in line {
+            match dir {
+                Direction::Open(kind) => {
+                    stack.push(kind);
+                },
+                Direction::Closed(kind) => {
+                    if stack.pop().unwrap() != kind {
+                        total += kind.syntax_score();
+                        continue 'outer;
+                    }
+                },
+            }
+        }
+    }
+
+    total.to_string()
+}
+
+pub fn part2(input: &mut dyn Read) -> String {
+    let stacks = get_inputs(input);
+
+    let mut totals = vec![];
+    'outer: for line in stacks {
+        let mut stack = Vec::new();
+        for dir in line {
+            match dir {
+                Direction::Open(kind) => {
+                    stack.push(kind);
+                },
+                Direction::Closed(kind) => {
+                    if stack.pop().unwrap() != kind {
+                        // ignore corrupt lines
+                        continue 'outer;
+                    }
+                },
+            }
+        }
+
+        let mut total = 0;
+        // our stack contains open kinds, and to close them, we must have closing kinds but _in reverse_
+        for elem in stack.iter().rev() {
+            total *= 5;
+            total += elem.auto_score();
+        }
+
+        totals.push(total);
+    }
+
+    let len = totals.len();
+    totals.select_nth_unstable(len / 2).1.to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::test_implementation;
+
+    use super::*;
+
+    const SAMPLE: &[u8] = include_bytes!("samples/10.txt");
+
+    #[test]
+    fn sample_part1() {
+        test_implementation(part1, SAMPLE, 26397);
+    }
+
+    #[test]
+    fn sample_part2() {
+        test_implementation(part2, SAMPLE, 288957);
+    }
 }
--- src/day11.rs
+++ src/day11.rs
@@ -1,9 +1,182 @@
-use std::io::Read;
+use std::fmt::Display;
+use std::io::{BufRead, BufReader, Read};
 
-pub fn part1(_input: &mut dyn Read) -> String {
-    todo!()
+fn sim_flash(grid: &mut Vec<Vec<u8>>, did_flash: &mut Vec<Vec<bool>>, y: usize, x: usize) {
+    assert!(!did_flash[y][x]);
+    did_flash[y][x] = true;
+    for dy in -1..=1 {
+        for dx in -1..=1 {
+            if dx == 0 && dy == 0 {
+                // Don't add one to self
+                continue;
+            }
+            let y = y as isize;
+            let x = x as isize;
+
+            let n_y = (y + dy);
+            let n_x = (x + dx);
+            if n_y < 0 || n_y >= grid.len() as isize {
+                continue;
+            }
+            if n_x < 0 || n_x >= grid[0].len() as isize {
+                continue;
+            }
+
+            let n_y = n_y as usize;
+            let n_x = n_x as usize;
+            grid[n_y][n_x] += 1;
+            if grid[n_y][n_x] > 9 && !did_flash[n_y][n_x] {
+                sim_flash(grid, did_flash, n_y, n_x);
+            }
+        }
+    }
 }
 
-pub fn part2(_input: &mut dyn Read) -> String {
-    todo!()
+// returns count of flashes
+fn sim_step(grid: &mut Vec<Vec<u8>>) -> usize {
+    let mut flashes = 0;
+
+    let mut did_flash = vec![vec![false; grid[0].len()]; grid.len()];
+
+    for y in 0..grid.len() {
+        for x in 0..grid[0].len() {
+            grid[y][x] += 1;
+
+            if grid[y][x] > 9 && !did_flash[y][x] {
+                // simulate neighbor's flash
+                sim_flash(grid, &mut did_flash, y, x);
+            }
+        }
+    }
+
+    print_grid(grid);
+    // println!();
+
+    for y in 0..grid.len() {
+        for x in 0..grid[0].len() {
+            if did_flash[y][x] {
+                grid[y][x] = 0;
+                flashes += 1;
+            }
+        }
+    }
+
+
+    flashes
+}
+
+fn print_grid(grid: &Vec<Vec<u8>>) {
+    print!("{}", termion::cursor::Goto(1,1));
+    // print!("{}{}", termion::clear::All, termion::cursor::Goto(1,1));
+    let colors: [Box<dyn Display>; 5] = [
+        Box::new(termion::color::Bg(termion::color::Black)),
+        Box::new(termion::color::Bg(termion::color::Rgb(40,40,40))),
+        Box::new(termion::color::Bg(termion::color::Rgb(80,80,80))),
+        Box::new(termion::color::Bg(termion::color::Rgb(150,150,150))),
+        Box::new(termion::color::Bg(termion::color::Rgb(200,200,200))),
+        // Box::new(termion::color::Bg(termion::color::LightBlue)),
+        // Box::new(termion::color::Bg(termion::color::Cyan)),
+        // Box::new(termion::color::Bg(termion::color::LightGreen)),
+        // &termion::color::Bg(termion::color::Blue),
+        // &termion::color::Bg(termion::color::LightBlue),
+        // &termion::color::Bg(termion::color::LightGreen),
+        // &termion::color::Bg(termion::color::White),
+    ];
+    for row in grid {
+        for &cell in row {
+            if cell <= 9 {
+                print!("{}  ", termion::color::Bg(termion::color::Rgb(255/10 * cell,255/10 * cell,255/10 * cell)));
+                // print!("{}.", colors[(cell/2) as usize]);
+                // print!("{}", cell);
+            } else {
+                print!("{}  ", termion::color::Bg(termion::color::Yellow));
+            }
+        }
+        println!();
+    }
+    print!("{}", termion::color::Bg(termion::color::Reset));
+    std::thread::sleep(std::time::Duration::from_millis(150));
+}
+
+fn grid_from_input(input: &mut dyn Read) -> Vec<Vec<u8>> {
+    BufReader::new(input)
+        .lines()
+        .map(|l| {
+            let l = l.unwrap();
+            l.chars().map(|c| {
+                c.to_digit(10).unwrap() as u8
+            }).collect()
+        }).collect()
+}
+
+pub fn part1(input: &mut dyn Read) -> String {
+    let mut grid = grid_from_input(input);
+    let mut flashes = 0;
+
+    for _ in 0..100 {
+        flashes += sim_step(&mut grid);
+    }
+
+    flashes.to_string()
+}
+
+pub fn part2(input: &mut dyn Read) -> String {
+    let mut grid = grid_from_input(input);
+
+    // clear grid
+    print!("{}", termion::clear::All);
+
+    // Not really used here
+    let mut steps = 0;
+
+    let max_flashes = grid.len() * grid[0].len();
+    let mut flashes = 0;
+    while flashes != max_flashes {
+        flashes = sim_step(&mut grid);
+        // let mut did_flash = vec![vec![false; grid[0].len()]; grid.len()];
+        // for y in 0..grid.len() {
+        //     for x in 0..grid[0].len() {
+        //         // in a step every octopus gets one energy
+        //         grid[y][x] += 1;
+        //
+        //         if grid[y][x] > 9 && !did_flash[y][x] {
+        //             // simulate neighbor's flash
+        //             sim_flash(&mut grid, &mut did_flash, y, x);
+        //         }
+        //     }
+        // }
+        //
+        // // reset all flashes
+        // for y in 0..grid.len() {
+        //     for x in 0..grid[0].len() {
+        //         if did_flash[y][x] {
+        //             grid[y][x] = 0;
+        //             flashes += 1;
+        //         }
+        //     }
+        // }
+
+        steps += 1;
+    }
+
+    steps.to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::test_implementation;
+
+    use super::*;
+
+    const SAMPLE: &[u8] = include_bytes!("samples/11.txt");
+
+    #[test]
+    fn sample_part1() {
+        test_implementation(part1, SAMPLE, 1656);
+    }
+
+    #[test]
+    fn sample_part2() {
+        test_implementation(part2, SAMPLE, 195);
+    }
 }
