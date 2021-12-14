ad0df736ab9ddc09ccf3d64f080cf0a8b87c244f
Niels Saurer <me@nielssaurer.com>
2021-12-14 23:41:03 +0100

    faster 14

--- src/day14.rs
+++ src/day14.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 use std::io::Read;
-
+use std::time::SystemTime;
 
 
 pub fn part1(input: &mut dyn Read) -> String {
@@ -70,96 +70,134 @@ pub fn part2(input: &mut dyn Read) -> String {
     input.read_to_string(&mut res).unwrap();
 
     let mut lines = res.lines();
-    let mut polymer = lines.next().unwrap().to_string();
-    lines.next().unwrap();
-    let mut rewrite_rules = lines.map(|l| {
-        let v = l.split(" -> ").collect::<Vec<_>>();
-        let pat = v[0].chars().collect::<Vec<_>>();
-        ([pat[0], pat[1]], v[1].chars().next().unwrap())
-    }).collect::<Vec<_>>();
 
-    let mut char_occs = HashMap::from([
-        ('A', 0),
-        ('B', 0),
-        ('C', 0),
-        ('D', 0),
-        ('E', 0),
-        ('F', 0),
-        ('G', 0),
-        ('H', 0),
-        ('I', 0),
-        ('J', 0),
-        ('K', 0),
-        ('L', 0),
-        ('M', 0),
-        ('N', 0),
-        ('O', 0),
-        ('P', 0),
-        ('Q', 0),
-        ('R', 0),
-        ('S', 0),
-        ('T', 0),
-        ('U', 0),
-        ('V', 0),
-        ('W', 0),
-        ('X', 0),
-        ('Y', 0),
-        ('Z', 0),
-    ]);
+    let mut char_occs = [0usize; 26];
+    let mut pattern_occs = [[0usize; 26]; 26];
 
-    for c in polymer.chars() {
-        *char_occs.get_mut(&c).unwrap() += 1;
-    }
+    let mut prev_char = None;
 
-    let rewrite_rules = rewrite_rules.into_iter().collect::<HashMap<_,_>>();
-
-    let mut pattern_occs = rewrite_rules.iter()
-        .map(|(&pat, _)| (pat, 0usize)).collect::<HashMap<_,_>>();
-
-    for wind in polymer.chars().collect::<Vec<_>>().windows(2) {
-        if let &[a, b] = wind {
-           *pattern_occs.entry([a, b]).or_insert(0) += 1;
-        } else {
-            unreachable!();
+    let mut polymer_chars = lines.next().unwrap().chars().map(|c| {
+        let c = c as u8 - b'A';
+        char_occs[c as usize] += 1;
+        if let Some(prev_char) = prev_char {
+            pattern_occs[prev_char as usize][c as usize] += 1;
         }
+        prev_char = Some(c);
+        c
+    }).collect::<Vec<u8>>();
+    lines.next().unwrap();
+
+    let mut rewrite_rules_map = [[26; 26]; 26];
+    for l in lines {
+        // let mut parts = l.split(" -> ");
+        let pat = &l[0..2]; //parts.next().unwrap();
+        let mut pat_chars = pat.chars();
+        let a = pat_chars.next().unwrap() as u8 - b'A';
+        let b = pat_chars.next().unwrap() as u8 - b'A';
+
+        let ins = l[6..7].chars().next().unwrap() as u8 - b'A';
+
+        rewrite_rules_map[a as usize][b as usize] = ins;
     }
+
+    // let mut rewrite_rules: Vec<([u8; 2], u8)> = lines.map(|l| {
+    //     let v = l.split(" -> ").collect::<Vec<_>>();
+    //     let pat = v[0].chars().collect::<Vec<_>>();
+    //     ([pat[0] as u8 - b'A', pat[1] as u8 - b'A'], v[1].chars().next().unwrap() as u8 - b'A')
+    // }).collect::<Vec<_>>();
+
+
+
+
+
+    // for &(pat, ins) in &rewrite_rules {
+    //     rewrite_rules_map[pat[0] as usize][pat[1] as usize] = ins;
+    // }
+
+    // let rewrite_rules = rewrite_rules.into_iter().collect::<HashMap<_,_>>();
+
+    // let mut pattern_occs = rewrite_rules.iter()
+    //     .map(|(&pat, _)| (pat, 0usize)).collect::<HashMap<_,_>>();
+
+
+    // for wind in polymer_chars.windows(2) {
+    //     if let &[a, b] = wind {
+    //        pattern_occs[a as usize][b as usize] += 1;
+    //     } else {
+    //         unreachable!();
+    //     }
+    // }
 
     // println!("{:?}", pattern_occs);
 
 
     for step in 1..=40 {
         // println!("Processing step {}", step);
+        let mut new_occs = [[0; 26]; 26];
 
-        let mut new_occs = HashMap::new(); //pattern_occs.clone();
-        for (&[a,b], &occ) in pattern_occs.iter() {
-            // println!("Processing pattern {:?}", [a,b]);
-            if let Some(&ins) = rewrite_rules.get(&[a,b]) {
-                // println!("Inserting {} inbetween {:?} which occured {} times", ins, [a,b], occ);
-                *char_occs.get_mut(&ins).unwrap() += occ;
-                // println!("char {} now has occs: {}", ins, char_occs[&ins]);
+        for a in 0..26 {
+            for b in 0..26 {
 
-                *new_occs.entry([a, ins]).or_insert(0) += occ;
-                *new_occs.entry([ins, b]).or_insert(0) += occ;
-
-                // new_occs.insert([a, ins], occ);
-                // new_occs.insert([ins, b], occ);
-            } else {
-                *new_occs.entry([a, b]).or_insert(0) += occ;
+                let ins = rewrite_rules_map[a as usize][b as usize];
+                if ins < 26 {
+                    let occ = pattern_occs[a as usize][b as usize];
+                    if occ > 0 {
+                        char_occs[ins as usize] += occ;
+                        new_occs[a as usize][ins as usize] += occ;
+                        new_occs[ins as usize][b as usize] += occ;
+                    }
+                }
+                // else {
+                //     // let occ = pattern_occs[a as usize][b as usize];
+                //     // never entered
+                //     // if occ > 0 {
+                //     //     new_occs[a as usize][b as usize] = occ;
+                //     // }
+                // }
             }
         }
 
-        // println!("char occs after all patterns: {:?}", char_occs);
-
         pattern_occs = new_occs;
+
+
+        // let mut new_occs = HashMap::new(); //pattern_occs.clone();
+        // for (&[a,b], &occ) in pattern_occs.iter() {
+        //     // println!("Processing pattern {:?}", [a,b]);
+        //     if let Some(&ins) = rewrite_rules.get(&[a,b]) {
+        //         // println!("Inserting {} inbetween {:?} which occured {} times", ins, [a,b], occ);
+        //         *char_occs.get_mut(&ins).unwrap() += occ;
+        //         // println!("char {} now has occs: {}", ins, char_occs[&ins]);
+        //
+        //         *new_occs.entry([a, ins]).or_insert(0) += occ;
+        //         *new_occs.entry([ins, b]).or_insert(0) += occ;
+        //
+        //         // new_occs.insert([a, ins], occ);
+        //         // new_occs.insert([ins, b], occ);
+        //     } else {
+        //         *new_occs.entry([a, b]).or_insert(0) += occ;
+        //     }
+        // }
+        //
+        // // println!("char occs after all patterns: {:?}", char_occs);
+        //
+        // pattern_occs = new_occs;
 
     }
 
 
 
-    let min = char_occs.iter().map(|(_, &occ)| occ).filter(|&occ| occ > 0).min().unwrap();
-    let max = char_occs.iter().map(|(_, &occ)| occ).max().unwrap();
+    let mut min = usize::MAX;
+    let mut max = usize::MIN;
 
+    for occ in char_occs {
+        if occ > 0 {
+            min = min.min(occ);
+        }
+        max = max.max(occ);
+    }
 
+    // let min = char_occs.iter().filter(|&&occ| occ > 0).min().unwrap();
+    // let max = char_occs.iter().max().unwrap();
 
     (max - min).to_string()
 }
